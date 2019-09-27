ea63f74e1fb047141c1004fd5d72c46ea74fa80a
edythawne <itsramires@gmail.com>
2019-09-27 12:45:35 -0500

    Initial Commit

---
+++ public/utils/db/ford32_insert_admons.sql
@@ -0,0 +1,14 @@
+	-- USE
+     USE Ford32_Servicios;
+	
+    INSERT INTO `Administradores` (`id_admin`, `nombre`, `apellidos`, `usuario`, `contrasena`, `created_at`, `update_at`) VALUES
+		(1, 'Edy', 'Thawne', '69d72300b5b4cf697087a892d35ba47518817cc6', '69d72300b5b4cf697087a892d35ba47518817cc6', '2019-09-09 09:53:19', NULL);
+	
+    INSERT INTO `Alumnos` (`id_alumno`, `nombre`, `apellidos`, `telefono`, `estado`, `especialidad`, `tipo_servicio`, `periodo`, `duracion`, `usuario`, `contrasena`, `created_at`, `update_at`) VALUES
+		(1, 'Charlotte', 'Schaizmen', 'SEJjWENlM0FCaVpYK0UvNWdHTm81dz09', 'ACTIVO', 'Programación', 'Servicio Social', 'Septiempre 2019 a Febrero 2020', 480, 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2019-09-09 09:53:19', NULL),
+		(2, 'Melanie', 'García', 'cWtGWnMrVFY4dzJqemQrQU5LWjRHdz09', 'ACTIVO', 'Administración', 'Servicio Social', 'Septiempre 2019 a Febrero 2020', 480, 'd5507c772bd098b984d804b229de023c5b73acdd', 'd5507c772bd098b984d804b229de023c5b73acdd', '2019-09-09 09:53:19', NULL),
+		(3, 'María Fernanda', 'Pérez Hernández', 'ZmxvdHVudUN3b0l1MHpMZEo4bEF2dz09', 'ACTIVO', 'Administración', 'Servicio Social', 'Septiempre 2019 a Febrero 2020', 480, 'fc5b0b882682e9981692cd55dc61622136a81d19', 'fc5b0b882682e9981692cd55dc61622136a81d19', '2019-09-09 09:53:19', NULL),
+		(4, 'Sarahí', 'García Florencia', 'eXZrMWFNYWZieGlPbkp1NGV2RW5tUT09', 'ACTIVO', 'Administración', 'Servicio Social', 'Septiempre 2019 a Febrero 2020', 480, '82fe9ea32ce3e5b93155c4125de5c1154a4a5daf', '82fe9ea32ce3e5b93155c4125de5c1154a4a5daf', '2019-09-09 09:53:19', NULL),
+		(5, 'Juan Carlos', 'Cortez Pérez', 'ZzFReWFKTnkwbWFCaCsxQlZjVnp0UT09', 'ACTIVO', 'Administración', 'Servicio Social', 'Septiempre 2019 a Febrero 2020', 480, '9793ac2f7291b701cdf9e49579d476149261e68d', '9793ac2f7291b701cdf9e49579d476149261e68d', '2019-09-09 09:53:19', NULL),
+		(6, 'Valentina', 'García Hernández', 'NjJvMEcyQmtOY1ZrZEk4cmRSbzJIdz09', 'ACTIVO', 'Alimentos y Bebidas', 'Servicio Social', 'Septiempre 2019 a Febrero 2020', 480, '7e0f2e9d156b52f5dd466b24b0f4c3ac4dc698d5', '7e0f2e9d156b52f5dd466b24b0f4c3ac4dc698d5', '2019-09-09 09:53:19', NULL),
+		(7, 'Nelson Michel', 'Martínez Pérez', 'blpiZ2krbTBUWGlmNEliMGxiTlQ0UT09', 'ACTIVO', 'Alimentos y Bebidas', 'Servicio Social', 'Septiembre 2019 - Febrero 2020', 480, 'f5c1585a51889753b6e899be0987f9de5634d699', 'f5c1585a51889753b6e899be0987f9de5634d699', '2019-09-09 09:54:35', NULL);
