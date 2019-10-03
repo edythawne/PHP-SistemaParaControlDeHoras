9e7866a35d50b86871a0e0117d281426fa3b6802
edythawne <itsramires@gmail.com>
2019-10-03 01:03:18 -0600

    Initial Commit

---
+++ public/utils/db/ford32_insert_horarios_03_oct.sql
@@ -0,0 +1,29 @@
+	-- USE
+	USE Ford32_Servicios;
+     
+	-- INSERT DE HORARIOS ALUMNO 02
+    INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+		(2, '2019-09-01 15:30:00', '2019-09-01 19:15:00', '2019-09-01 15:30:00', '2019-09-01 19:15:00'),
+        (2, '2019-09-02 15:30:00', '2019-09-02 19:15:00', '2019-09-02 15:30:00', '2019-09-02 19:45:00');
+    
+    -- INSERT DE HORARIOS ALUMNO 03
+    INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+    	(3, '2019-09-01 15:30:00', '2019-09-01 19:15:00', '2019-09-01 15:30:00', '2019-09-01 19:15:00'),
+        (3, '2019-09-02 15:30:00', '2019-09-02 19:15:00', '2019-09-02 15:30:00', '2019-09-02 19:45:00');
+        
+    -- INSERT DE HORARIOS ALUMNO 04
+    -- INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES ();
+    
+    -- INSERT DE HORARIOS ALUMNO 05
+    INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+		(5, '2019-09-01 15:30:00', '2019-09-01 19:15:00', '2019-09-01 15:30:00', '2019-09-01 19:15:00'),
+        (5, '2019-09-02 15:30:00', '2019-09-02 19:30:00', '2019-09-02 15:30:00', '2019-09-02 19:30:00'),
+        (5, '2019-09-03 15:30:00', '2019-09-03 19:30:00', '2019-09-03 15:30:00', '2019-09-03 19:15:00');
+        
+    -- INSERT DE HORARIOS ALUMNO 06
+    INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+		(6, '2019-09-01 14:00:00', '2019-09-01 19:15:00', '2019-09-01 14:00:00', '2019-09-01 19:15:00'),
+        (6, '2019-09-03 17:00:00', '2019-09-03 19:15:00', '2019-09-03 17:00:00', '2019-09-03 19:15:00');
+    
+    -- INSERT DE HORARIOS ALUMNO 07    
+    -- INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES ();
