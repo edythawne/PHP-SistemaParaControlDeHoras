f1808d9defb3cb607216dc7f241326b66cd282f5
edythawne <itsramires@gmail.com>
2020-02-05 15:50:09 -0600

    Initial Commit

---
+++ public/utils/db/ford32_insert_horarios_07_feb.sql
@@ -0,0 +1,24 @@
+-- USE
+USE Ford32_Servicios;
+
+-- INSERT DE HORARIOS ALUMNO 02
+INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+    (2, '2020-02-04 14:00:00', '2020-02-04 18:00:00', '2020-02-04 14:45:00', '2020-02-04 19:00:00');
+
+-- INSERT DE HORARIOS ALUMNO 03
+INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+    (3, '2020-02-04 14:00:00', '2020-02-04 18:00:00', '2020-02-04 14:45:00', '2020-02-04 19:00:00');
+
+    
+        
+-- INSERT DE HORARIOS ALUMNO 04
+
+    
+-- INSERT DE HORARIOS ALUMNO 05
+INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+    (5, '2020-02-04 14:00:00', '2020-02-04 18:00:00', '2020-02-04 14:45:00', '2020-02-04 19:00:00');
+
+    
+-- INSERT DE HORARIOS ALUMNO 06
+INSERT INTO Horarios (fk_alumno, entrada, salida, al_entrada, al_salida) VALUES
+    (6, '2020-02-04 14:00:00', '2020-02-04 18:00:00', '2020-02-04 14:45:00', '2020-02-04 19:00:00');
